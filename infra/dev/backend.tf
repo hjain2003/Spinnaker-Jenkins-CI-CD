412e34ba17640648cef20fbbcdadcf5ad290b4c7
root <root@ip-172-31-23-74.us-west-1.compute.internal>
2025-08-24 04:52:16 +0000

    tf change

--- infra/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "mini-cicd-tfstate-<yourid>"
-    key    = "dev/terraform.tfstate"
-    region = "us-west-1"
-    dynamodb_table = "terraform-locks"
-    encrypt = true
-  }
-}
